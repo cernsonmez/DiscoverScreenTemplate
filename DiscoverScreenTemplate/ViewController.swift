026eda8a2bf6c2fcd2acf0d40b06735a415a80f1
Ceren Sonmez <cerensonmez95@gmail.com>
2022-10-20 21:58:52 +0300

    create a basic discover screen with collection view

--- DiscoverScreenTemplate/ViewController.swift
+++ DiscoverScreenTemplate/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as? MainCollectionViewCell {
+            
+            cell.imageView.image = UIImage(named: "new_picture")
+            return cell
+            
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = self.view.frame.width - 16.0 * 2
+        let height: CGFloat = 234.0
+        
+        return CGSize(width: width, height: height)
+    }
+}
